6429da7b3e5d95089418ab806509b97f936c256a
freshlin <15198041153@qq.com>
2016-07-27 16:03:45 +0800

    sdsd

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.administrator.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements Runnable{
 
+    private int x = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,9 +16,28 @@ public class MainActivity extends Activity{
         int xx = 1;
         int yy = 2;
         int zz = 3;
-
         int qq = 4;
-
         int uu = 2;
+
+        this.run();
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+
+
+
+    @Override
+    public void run() {
+
+        x += 1;
+
+        handler.postDelayed(this, 3000);
+
+        Log.e("测试", String.valueOf(x));
     }
 }
